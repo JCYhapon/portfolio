3de511758042f46990c3961a5afebd08750cc78e
JCYhapon <yhaponjc@gmail.com>
2024-05-13 14:26:25 +0800

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <Header name="John Carlo Yhapon" />
+        <Navbar navbar={["Overview", "Skills", "Projects", "Contact"]} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <span>Hello my name is {props.name}</span>
+    </div>
+  );
+};
+
+export default Header;
