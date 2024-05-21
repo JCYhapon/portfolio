cf9aae7d16adee094d9c703f17bf5a84227010e9
JCYhapon <yhaponjc@gmail.com>
2024-05-21 21:43:50 +0800

    modified: practice for child parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Main = () => {
+  return (
+    <div>
+      haha
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
