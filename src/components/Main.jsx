693852e50f264c86fffba75cd9c4bfda15417f09
JCYhapon <yhaponjc@gmail.com>
2024-05-22 15:32:23 +0800

    modified: layout the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
 
 const Main = () => {
   return (
-    <section className=" min-h-screen bg-[#1B2127] p-4 text-white">
-      <Header />
+    <section className=" min-h-screen bg-[#1B2127] py-6 px-12 text-[#efeee5]">
+      {/* HEADER */}
+      <div>
+        <Header />
+      </div>
+
+      <div className="mt-[5rem] flex w-full h-[75vh] ">
+        <div className="w-[20%]">
+          <Sidebar />
+        </div>
+        <div className="w-[80%] border border-[#6E7681] rounded-md">
+          MAIN CONTENT HERE
+        </div>
+      </div>
     </section>
   );
 };
