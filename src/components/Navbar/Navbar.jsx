c25d8d8c0527edb2a0aa9a13b9cec1dc9c145eec
JCYhapon <yhaponjc@gmail.com>
2024-05-22 15:34:54 +0800

    removed: unused codes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,7 @@ const Navbar = (props) => {
   return (
     <>
       <nav>
-        <ul>
-          <li>{props.navbar[0]}</li>
-          <li>{props.navbar[1]}</li>
-          <li>{props.navbar[2]}</li>
-          <li>{props.navbar[3]}</li>
-        </ul>
+        
       </nav>
     </>
   );
