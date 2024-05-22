d7dd30c3704a9ab46fc31f2d4c2ff56af4416971
JCYhapon <yhaponjc@gmail.com>
2024-05-22 13:51:45 +0800

    removed unused codes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from "./Header";
+import Header from "./Header/Header";
 
 const Main = () => {
   return (
-    <div>
-      haha
+    <section className=" min-h-screen bg-[#1B2127] p-4 text-white">
       <Header />
-    </div>
+    </section>
   );
 };
 
