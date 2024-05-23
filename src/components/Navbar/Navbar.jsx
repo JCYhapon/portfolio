e76b19ca8e9d12b4a7f49f574567144d8a56591f
JCYhapon <yhaponjc@gmail.com>
2024-05-23 22:32:08 +0800

    no icons yet, but all done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
 
-const Navbar = (props) => {
+const Navbar = ({ navbar, setSelectedSection }) => {
+  const [selectedItem, setSelectedItem] = useState("Overview");
+
+  const handleClick = (item) => {
+    setSelectedSection(item);
+    setSelectedItem(item);
+  };
+
   return (
-    <>
-      <nav>
-        
-      </nav>
-    </>
+    <nav className="w-full bg-[#0D1117] h-[5rem] rounded-t-md overflow-hidden">
+      <ul id="navbar">
+        {navbar.map((item) => (
+          <li
+            key={item}
+            onClick={() => handleClick(item)}
+            className={selectedItem === item ? "active" : ""}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
