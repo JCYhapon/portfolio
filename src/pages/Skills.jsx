8e3ead5030f35826bb758580b002876c10509c3c
JCYhapon <yhaponjc@gmail.com>
2024-05-23 22:30:53 +0800

    modified

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="px-[35px]">
+      <div className=" mt-[1rem]">
+        <h1 className="text-[55px]">Skills and Technologies</h1>
+        <p className="   text-[20px] mx-auto font-light text-gray-300">
+          These are the main languages and technologies I use for website
+          development.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
