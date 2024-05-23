4ef1b4e5d6fe562b754d3c045e2c5e5a822d24ea
JCYhapon <yhaponjc@gmail.com>
2024-05-23 22:31:36 +0800

    modified

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="px-[35px]">
+      <div className=" mt-[1rem]">
+        <h1 className="text-[55px]">Let's Work Together</h1>
+        <p className="   text-[20px] mx-auto font-light text-gray-300">
+          Want to build something cool? Send me an email.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
