d2e7214a863e5b01e2915bb60bb784527c4540b8
JCYhapon <yhaponjc@gmail.com>
2024-05-25 20:47:09 +0800

    structured

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import Navbar from "./Navbar/Navbar";
+import Overview from "../pages/Overview";
+import Skills from "../pages/Skills";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
 
 const Main = () => {
+  const [selectedSection, setSelectedSection] = useState("Overview");
+
+  const renderSection = () => {
+    switch (selectedSection) {
+      case "Overview":
+        return <Overview />;
+      case "Skills":
+        return <Skills />;
+      case "Projects":
+        return <Projects />;
+      case "Contact":
+        return <Contact />;
+      default:
+        return <Overview />;
+    }
+  };
+
   return (
-    <section className=" min-h-screen bg-[#1B2127] py-6 px-12 text-[#efeee5]">
+    <section className="min-h-screen bg-[#1B2127] py-6 px-12 text-[#efeee5]">
       {/* HEADER */}
       <div>
         <Header />
@@ -15,7 +37,11 @@ const Main = () => {
           <Sidebar />
         </div>
         <div className="w-[80%] border border-[#6E7681] rounded-md">
-          MAIN CONTENT HERE
+          <Navbar
+            navbar={["Overview", "Skills", "Projects", "Contact"]}
+            setSelectedSection={setSelectedSection}
+          />
+          <div className="p-4">{renderSection()}</div>
         </div>
       </div>
     </section>
