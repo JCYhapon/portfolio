13f7d9d609f192fbca424f5bd83928db677359a5
JCYhapon <yhaponjc@gmail.com>
2024-05-26 15:40:13 +0800

    import/export all the svg to make it clean code

--- src/assets/Skills/index.js
+++ src/assets/Skills/index.js
@@ -1,4 +1,27 @@
 import Angular from "../Skills/Angular.svg";
+import Bootstrap from "../Skills/Bootstrap.svg";
+import Firebase from "../Skills/Firebase.svg";
+import Git from "../Skills/Git.svg";
+import Github from "../Skills/Github.svg";
 import Javascript from "../Skills/Javascript.svg";
+import Laravel from "../Skills/Laravel.svg";
+import Mysql from "../Skills/Mysql.svg";
+import PHP from "../Skills/Php.svg";
+import Reactjs from "../Skills/React.svg";
+import Tailwindcss from "../Skills/Tailwindcss.svg";
+import Wordpress from "../Skills/Wordpress.svg";
 
-export { Angular, Javascript };
+export {
+  Angular,
+  Bootstrap,
+  Firebase,
+  Git,
+  Github,
+  Javascript,
+  Laravel,
+  Mysql,
+  PHP,
+  Reactjs,
+  Tailwindcss,
+  Wordpress,
+};
