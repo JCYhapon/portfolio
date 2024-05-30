3ffd434af67d08826e63bbee922e05951c9a7a44
JCYhapon <yhaponjc@gmail.com>
2024-05-30 23:01:51 +0800

    modified: added different screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "360px", // Extra small screens (e.g., small mobile phones)
+        sm: "640px", // Small screens (e.g., mobile phones)
+        md: "768px", // Medium screens (e.g., tablets)
+        lg: "1024px", // Large screens (e.g., laptops)
+        xl: "1280px", // Extra large screens (e.g., desktops)
+        "2xl": "1536px", // 2XL screens
+      },
+    },
   },
   plugins: [],
 };
