8ffbebad6d2c3a89399f3d7408a15f6dd85c73b3
JCYhapon <yhaponjc@gmail.com>
2024-05-30 23:04:07 +0800

    modified: mobile responsive, not yet on other screen size

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Overview = () => {
   return (
-    <section className="px-[35px]">
+    <section className="lg:px-[35px] px-[10px]">
       <div className=" mt-[1rem]">
-        <h1 className="text-[55px]">Hey there! 👋🏼</h1>
-        <p className="  mt-[50px] text-[20px] mx-auto font-light text-gray-300">
+        <h1 className="lg:text-[55px] text-[35px]">Hey there! 👋🏼</h1>
+        <p className="lg:mt-[50px] mt-[25px] lg:text-[20px] mx-auto font-light text-gray-300">
           I'm Yhaps, a frontend developer on a mission to redefine online
           experiences one website at a time. With a keen eye for design and a
           passion for innovation, I pour my heart and soul into crafting dynamic
           digital worlds that not only meet but exceed the expectations of
           businesses and users alike.
         </p>
-        <p className="mt-[20px] text-[20px] mx-auto font-light text-gray-300">
+        <p className="mt-[20px] lg:text-[20px] mx-auto font-light text-gray-300">
           My journey in the world of web development is an adventure filled with
           endless possibilities. I thrive on the thrill of exploring diverse
           programming languages, each one offering its own unique set of tools
@@ -23,18 +23,22 @@ const Overview = () => {
         </p>
       </div>
 
-      <div className="mt-[5rem] flex gap-[12rem] ">
+      <div className="lg:mt-[5rem] mt-[2rem] flex lg:gap-[12rem] gap-6">
         <div className="flex flex-col items-center">
-          <span className="text-[50px] text-[#64DD50]">+2</span>
-          <span>Years of experience</span>
+          <span className="lg:text-[50px] text-[40px] text-[#64DD50]">+2</span>
+          <span className="xs:text-[10px] lg:text-[16px]">
+            Years of experience
+          </span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="text-[50px] text-[#64DD50]">+10</span>
-          <span>Build website projects</span>
+          <span className="lg:text-[50px] text-[40px] text-[#64DD50]">+10</span>
+          <span className="xs:text-[10px] lg:text-[16px]">
+            Build website projects
+          </span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="text-[50px] text-[#64DD50]">+5</span>
-          <span>Certifications</span>
+          <span className="lg:text-[50px] text-[40px] text-[#64DD50]">+5</span>
+          <span className="xs:text-[10px] lg:text-[16px]">Certifications</span>
         </div>
       </div>
     </section>
