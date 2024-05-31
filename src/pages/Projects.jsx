ef2d37f77f4c39df344e8c5d8c5664ff1701a21c
JCYhapon <yhaponjc@gmail.com>
2024-05-31 11:20:20 +0800

    modified: removed unused codes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,36 +5,32 @@ import SbaojtPortal from "../assets/Projects/sbaojt-portal.png";
 
 const Projects = () => {
   return (
-    <section className="px-[35px]">
-      <div className=" mt-[1rem]">
-        <div className="flex justify-between items-center">
-          <h1 className="text-[55px]">Featured Projects</h1>
-          <button
-            className="flex align-items-center items-center bg-[#64DD50] text-black p-1 w-[8rem] justify-center gap-1 rounded-lg font-semibold"
-            id="view-projects"
-          >
-            View All Projects
-          </button>
+    <section className="lg:px-[35px] xs:px-[10px]">
+      <div className="lg:flex justify-between mt-[1rem]">
+        <div className=" ">
+          <h1 className="lg:text-[55px] text-[25px]">Featured Projects</h1>
+          <p className="lg:text-[20px] mx-auto font-light text-gray-300">
+            These are the projects I've crafted with the mentioned technologies,
+            and I find joy in working with them.
+          </p>
         </div>
 
-        <p className="text-[20px] mx-auto font-light text-gray-300">
-          These are the projects I've crafted with the mentioned technologies,
-          and I find joy in working with them.
-        </p>
+        <button
+          className="flex lg:my-0 lg:mb-0 xs:mb-[3rem] xs:my-6 h-[45px] items-center bg-[#64DD50] text-black p-1 w-[8rem] justify-center gap-1 rounded-lg font-semibold"
+          id="view-projects"
+        >
+          View All Projects
+        </button>
       </div>
 
-      <div className="mt-6 flex justify-between">
+      <div className="mt-6 flex xs:flex-col xs:gap-[3rem] lg:flex-row lg:justify-between">
         {/* PROJECT 1 */}
-        <div className="w-[400px] flex flex-col">
-          <img
-            src={SbaojtPortal}
-            alt=""
-            className="border w-auto h-[200px] rounded"
-          />
+        <div className="lg:w-[400px] flex flex-col ">
+          <img src={SbaojtPortal} alt="" className="border h-[200px] rounded" />
           <h2 className="text-[20px] font-bold mt-3" id="title">
             SBA-OJT Portal
           </h2>
-          <p className="details mt-2 text-[#babec4]">
+          <p className="details mt-2 text-[#babec4] ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
@@ -60,7 +56,7 @@ const Projects = () => {
         </div>
 
         {/* PROJECT 2 */}
-        <div className="w-[400px] flex flex-col">
+        <div className="lg:w-[400px] flex flex-col">
           <img
             src={KapampanganHistory}
             alt="Kapampangan History"
@@ -95,7 +91,7 @@ const Projects = () => {
         </div>
 
         {/* PROJECT 3 */}
-        <div className="w-[400px] flex flex-col">
+        <div className="lg:w-[400px] flex flex-col">
           <img src="" alt="" className="border w-auto h-[200px] rounded" />
           <h2 className="text-[20px] font-bold mt-3" id="title">
             Event Tracker
@@ -104,7 +100,7 @@ const Projects = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <div className="flex gap-2 mt-3">
+          <div className="flex gap-2 mt-3 flex-wrap">
             <span className="rounded-full  py-[2px] px-[15px]  border ">
               HTML
             </span>
