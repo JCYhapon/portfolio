e7b8b7918edaac5a54ddf1b6544f79675b652487
JCYhapon <yhaponjc@gmail.com>
2024-05-31 11:28:02 +0800

    layout: mobile responsive | no content yet

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section className="px-[35px]">
+    <section className="lg:px-[35px] xs:px-[10px]">
       <div className=" mt-[1rem]">
-        <h1 className="text-[55px]">Let's Work Together</h1>
-        <p className="   text-[20px] mx-auto font-light text-gray-300">
+        <h1 className="lg:text-[55px] text-[25px]">Let's Work Together</h1>
+        <p className="lg:text-[20px] mx-auto font-light text-gray-300">
           Want to build something cool? Send me an email.
         </p>
       </div>
