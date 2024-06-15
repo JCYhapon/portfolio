86989a61e0bca02e86d27af3593e31a189dd45f3
JCYhapon <yhaponjc@gmail.com>
2024-06-15 16:36:46 +0800

    imported new images for projects

---
+++ src/assets/AllProjects/index.js
@@ -0,0 +1,8 @@
+import Portfolio from "./portfolio.png";
+import Modern from "./modern.png";
+import Udays from "./udays.png";
+import Dual from "./dual-portfolio.png";
+import Guess from "./guess-my-number.png";
+import BMI from "./bmi-calculator.png";
+
+export { Portfolio, Modern, Udays, Dual, Guess, BMI };
