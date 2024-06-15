a05f83d356b36fb540804515b719a7a5b65e9826
JCYhapon <yhaponjc@gmail.com>
2024-06-15 16:36:00 +0800

    added: other projects and images

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import {
+  Portfolio,
+  Modern,
+  Udays,
+  Dual,
+  Guess,
+  BMI,
+} from "../assets/AllProjects/index";
+
+export const allprojects = [
+  {
+    id: "project-1",
+    image: Portfolio,
+    title: "Personal Portfolio",
+    description:
+      "This personal portfolio was built to showcase my projects and the journey of my web development. Here, you'll find a curated collection of frontend and backend projects that demonstrate my passion for crafting innovative solutions and my commitment to continuous learning in the ever-evolving world of web development. ",
+    skills: ["HTML", "CSS", "JavaScript"],
+    liveSite: "https://jcyhapon.github.io/",
+    githubLink: "https://github.com/JCYhapon/JCYhapon.github.io",
+  },
+  {
+    id: "project-2",
+    image: Modern,
+    title: "Modern Application",
+    description:
+      "It's a modern clone app that lets users browse through various landing pages, providing a hands-on way to learn and showcase what I've learned in web development. Whether it's experimenting with new features or refining design techniques, this project is a platform for creativity and growth in building web applications.",
+    skills: ["JavaScript", "ReactJS", "CSS"],
+    liveSite: "https://modern-hoobank-clone-mu.vercel.app/",
+    githubLink: "https://github.com/JCYhapon/modern-app",
+  },
+  {
+    id: "project-3",
+    image: Udays,
+    title: "University Days",
+    description:
+      "A web application meticulously crafted to showcase the vibrant tapestry of events and moments from my university journey in 2023. It serves as a digital memoir, capturing everything from academic milestones to memorable campus gatherings. Through this platform, users can relive the excitement of campus life, explore various events.",
+    skills: ["HTML", "CSS", "JavaScript", "AWS"],
+    liveSite: "https://udays-rho.vercel.app/",
+    githubLink: "https://github.com/JCYhapon/udays",
+  },
+  {
+    id: "project-4",
+    image: Dual,
+    title: "Dual Portfolio",
+    description:
+      "A web application meticulously crafted to showcase the vibrant tapestry of events and moments from my university journey in 2023. It serves as a digital memoir, capturing everything from academic milestones to memorable campus gatherings. Through this platform, users can relive the excitement of campus life, explore various events.",
+    skills: ["HTML", "TailwindCSS", "JavaScript", "App Script", "Integration"],
+    liveSite: "https://udays-rho.vercel.app/",
+    githubLink: "https://github.com/JCYhapon/udays",
+  },
+  {
+    id: "project-5",
+    image: Guess,
+    title: "Guess my number!",
+    description:
+      "A web application meticulously crafted to showcase the vibrant tapestry of events and moments from my university journey in 2023. It serves as a digital memoir, capturing everything from academic milestones to memorable campus gatherings. Through this platform, users can relive the excitement of campus life, explore various events.",
+    skills: ["HTML", "CSS", "JavaScript", "AWS"],
+    liveSite: "https://udays-rho.vercel.app/",
+    githubLink: "https://github.com/JCYhapon/udays",
+  },
+  {
+    id: "project-5",
+    image: BMI,
+    title: "BMI Calculator",
+    description:
+      "A web application meticulously crafted to showcase the vibrant tapestry of events and moments from my university journey in 2023. It serves as a digital memoir, capturing everything from academic milestones to memorable campus gatherings. Through this platform, users can relive the excitement of campus life, explore various events.",
+    skills: ["HTML", "CSS", "JavaScript", "AWS"],
+    liveSite: "https://udays-rho.vercel.app/",
+    githubLink: "https://github.com/JCYhapon/udays",
+  },
+];
