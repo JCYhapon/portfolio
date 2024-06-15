0f40b2ff707bf192a1bc7c2541c7d5699431ea81
JCYhapon <yhaponjc@gmail.com>
2024-06-15 19:52:00 +0800

    layour: responsive in all devices

--- src/pages/AllProjects.jsx
+++ src/pages/AllProjects.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const AllProjects = () => {
   return (
-    <div className="xl:px-[15rem] lg:px-[10rem] py-[3rem] min-h-screen text-[#efeee5]">
-      <section className="flex justify-between align-items-center">
+    <div className="xl:px-[15rem] lg:px-[10rem] md:px-[8rem] sm:px-[5rem] xs:px-[2rem] py-[3rem] min-h-screen text-[#efeee5]">
+      <section className="flex justify-between items-center">
         <Link to="/home">
           <button className="hover:underline">Back to portfolio</button>
         </Link>
@@ -16,12 +16,12 @@ const AllProjects = () => {
 
       <section className="">
         <div className="mb-[4rem]">
-          <h1 className="text-[3rem] mb-5 relative">
+          <h1 className="text-[3rem] mb-5 relative mt-[3rem]">
             PROJECTS
             <span className="absolute bottom-0 left-0 w-[20%] h-1 bg-[#64dd50]"></span>
           </h1>
 
-          <p className="w-[80%] text-[18px] font-light">
+          <p className="xl:w-[80%] xl:text-[18px] font-light">
             Here’s a collection of all the projects I’ve created using a variety
             of programming languages, spanning both frontend and backend
             development. Each project is a testament to my journey, showcasing
@@ -42,9 +42,7 @@ const AllProjects = () => {
                 className="w-full h-auto rounded-md mb-4 border"
               />
               <h2 className="text-[20px] font-bold mt-3">{project.title}</h2>
-              <p className="mt-2 text-[#babec4] text-justify">
-                {project.description}
-              </p>
+              <p className="mt-2 text-[#babec4] ">{project.description}</p>
               <div className="flex flex-wrap gap-2">
                 {project.skills.map((skill) => (
                   <span
