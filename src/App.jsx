d59074f5c7552eff2b3a777f581642cac3e56472
JCYhapon <yhaponjc@gmail.com>
2024-06-15 13:54:01 +0800

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Main from "./components/Main";
+import AllProjects from "./pages/AllProjects";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/all-projects" element={<AllProjects />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
