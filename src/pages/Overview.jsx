8ff4d4c27f08e0250ecae33f882f6609fb5ce31d
Code <98503854+JCYhapon@users.noreply.github.com>
2026-06-10 10:35:48 +0800

    Update Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -8,11 +8,9 @@ const Overview = () => {
           Hey there! 👋🏼
         </h1>
         <p className="xl:mt-[50px] mt-[25px] xl:text-[20px] lg:text-[20px] mx-auto font-light text-gray-300">
-          I'm Yhaps, a frontend developer on a mission to redefine online
-          experiences one website at a time. With a keen eye for design and a
-          passion for innovation, I pour my heart and soul into crafting dynamic
-          digital worlds that not only meet but exceed the expectations of
-          businesses and users alike.
+          I'm Yhaps, Web Developer with 3+ years of experience building responsive, high-performance WordPress websites and
+          WooCommerce solutions. Proficient in Elementor Pro, Advanced Custom Fields, Laravel, PHP, and ReactJS. Proven
+          ability to deliver polished, business-ready websites from concept to launch — on time and to spec.
         </p>
         <p className="mt-[20px] xl:text-[20px] lg:text-[20px] mx-auto font-light text-gray-300">
           My journey in the world of web development is an adventure filled with
@@ -27,13 +25,13 @@ const Overview = () => {
 
       <div className="xl:mt-[5rem] lg:mt-[3rem] flex xl:gap-[12rem] lg:gap-[6rem] gap-6">
         <div className="flex flex-col items-center">
-          <span className="xl:text-[50px] text-[40px] text-[#64DD50]">+2</span>
+          <span className="xl:text-[50px] text-[40px] text-[#64DD50]">+3</span>
           <span className="xs:text-[10px] xl:text-[16px] lg:text-[16px]">
             Years of experience
           </span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="xl:text-[50px] text-[40px] text-[#64DD50]">+10</span>
+          <span className="xl:text-[50px] text-[40px] text-[#64DD50]">+40</span>
           <span className="xs:text-[10px] xl:text-[16px] lg:text-[16px]">
             Build website projects
           </span>
